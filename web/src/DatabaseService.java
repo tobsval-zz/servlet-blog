adc14e23717f6e6729fb60e5ab422717d40bcb85
Tobia Valerio <tobiavalerio.studio@gmail.com>
2019-03-13 16:42:19 +0100

    - Optimized LogoutServlet

--- web/src/DatabaseService.java
+++ web/src/DatabaseService.java
@@ -33,17 +33,4 @@ class DatabaseService {
         return foundEntry;
     }
 
-
-    ResultSet forwardCustomQuery(PreparedStatement preparedStatement) throws SQLException {
-        try(Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/blog-db")) {
-
-            ResultSet prepStmtResultSet = preparedStatement.getResultSet();
-            preparedStatement.close();
-            prepStmtResultSet.close();
-
-            return prepStmtResultSet;
-
-        }
-    }
-
 }
--- web/src/LogoutServlet.java
+++ web/src/LogoutServlet.java
@@ -2,18 +2,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 
 public class LogoutServlet extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try (PrintWriter serverOut = response.getWriter()) {
-            HttpSession userSession = request.getSession();
-            userSession.invalidate();
-            response.setHeader("Refresh", "1;url=/index.jsp");
-        }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession userSession = request.getSession();
+        userSession.invalidate();
+        response.setHeader("Refresh", "0;url=/index.jsp"); //Redirect to home
     }
 
 }
